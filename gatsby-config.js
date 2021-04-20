d3061338b13d458631badcf1000c6a45a5aad4a3
Marcin Pasiewicz <marcin.pasiewicz@acoustic.co>
2021-04-20 12:37:32 +0200

    feat(api): add content api request as custom data source

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,14 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: "gatsby-source-custom-api",
+      options: {
+        url:
+          "https://content-eu-4.content-cms.com/api/859f2008-a40a-4b92-afd0-24bb44d10124/delivery/v1/search?q=text:(**)&fl=document:[json]&fq=libraryId:(d7094af2\-8966\-40db\-8803\-c83d663810f8)",
+      },
+    },
+  ],
+};
+
